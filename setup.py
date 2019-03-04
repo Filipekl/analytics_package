c47d21af7b8a3582986fd9a90ce5cc90d8429da9
Lukasz Filipek <lukasz.filipek@tmsw.com>
2019-03-04 14:05:01 -0600

    added setup.py file to install package

---
+++ setup.py
@@ -0,0 +1,9 @@
+
+from setuptools import setup
+
+setup(name="analytics_package",
+      version='0.1',
+      description='Custom functionality to make data science easier and more reproducible',
+      author='Lukasz Filipek',
+      packages=['analytics_package'],
+      zip_safe=False)
